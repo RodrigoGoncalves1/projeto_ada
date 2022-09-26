cea6108703463ee2e2f442c8426c7b826d7c1752
Juliana-darwin <113155938+juliana-darwin@users.noreply.github.com>
2022-09-26 19:14:02 -0300

    deletando partes sem uso do site
    deletando partes sem uso e atualizando titulo.

--- js/custom.js
+++ js/custom.js
@@ -113,6 +113,40 @@
 			})
 		}
 
+		var destinationSlider = document.querySelectorAll('.destination-slider-2');
+
+		if ( destinationSlider.length > 0 ) {
+			var desSlider = tns({
+				container: ".destination-slider-2",
+				mouseDrag: true,
+				items: 1,
+				axis: "horizontal",
+				swipeAngle: false,
+				speed: 700,
+				edgePadding: 50,
+				nav: true,
+				gutter: 30,
+				autoplay: true,
+				autoplayButtonOutput: false,
+				controlsContainer: "#destination-controls",
+				responsive: {
+					350: {
+						items: 1
+					},
+					
+					500: {
+						items: 2
+					},
+					600: {
+						items: 3
+					},
+					900: {
+						items: 5
+					}
+				},
+			})
+		}
+
 
 
 	}
