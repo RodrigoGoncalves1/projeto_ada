d45a18c0d53793cc92cb6f530c84cd7a7d44af89
Juliana-darwin <113155938+juliana-darwin@users.noreply.github.com>
2022-09-27 17:09:32 -0300

    videos e fotos
    fotos e videos da ia das crianças e a Zoe falando ingles

--- js/custom.js
+++ js/custom.js
@@ -147,6 +147,42 @@
 			})
 		}
 
+		var destinationSlider = document.querySelectorAll('.destination-slider-3');
+
+		if ( destinationSlider.length > 0 ) {
+			var desSlider = tns({
+				container: ".destination-slider-3",
+				mouseDrag: true,
+				items: 1,
+				axis: "horizontal",
+				swipeAngle: false,
+				speed: 700,
+				edgePadding: 50,
+				nav: true,
+				gutter: 30,
+				autoplay: true,
+				autoplayButtonOutput: false,
+				controlsContainer: "#destination-controls",
+				responsive: {
+					350: {
+						items: 1
+					},
+					
+					500: {
+						items: 2
+					},
+					600: {
+						items: 3
+					},
+					900: {
+						items: 5
+					}
+				},
+			})
+		}
+
+
+
 
 
 	}
